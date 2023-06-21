a0d8326dc7fce3f783ec5d3aaf6349ff596b1e7a
Kai Prenger <kai.prenger@gmail.com>
2023-06-21 18:06:51 -0400

    binding markers and tooltips

--- 6-21/my_site/script.js
+++ 6-21/my_site/script.js
@@ -1,1 +1,24 @@
 console.log('loading: loaded')
+
+let map = L.map('map').setView([45.604, -114.143], 5.91)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
+
+// thus ends 00
+
+// thus begins 01
+const marker = L.marker([43.529073, -116.065394]).addTo(map)
+
+marker.bindPopup("<b>City Name</b><br>Sandy Point")
+
+// thus ends 01
+
+// thus begins 02
+
+
+
+
+
