848f83430042f5f8a96ee5f839e4e04e2afc92b6
Kai Prenger <kai.prenger@gmail.com>
2023-07-08 15:54:40 -0400

    changes to make filters work, CSS and HTML work to format nicely

--- 6-21/my_site/script.js
+++ 6-21/my_site/script.js
@@ -3,83 +3,102 @@ console.log('loading: loaded')
 let map = L.map('map').setView([45.604, -114.143], 5.91)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
+let activeFilters = new Set([
+  'AIR VALVE', 
+  'CATTLEGUARD', 
+  'DRAIN', 
+  'GATE',
+  'PRESSURE BREAK',
+  'SPRING',
+  'TROUGH',
+  'VALVE'
+])
 
+const filterFeatures = (feature) => {
+  return activeFilters.has(feature.properties.POINT_FEAT) &&
+  (
+    feature.properties.PROJ_NAME === 'Shares Basin Fence' ||
+    feature.properties.PROJ_NAME === 'Jim Sage Cattleguard' || 
+    feature.properties.PROJ_NAME === 'Robber Gulch Pipeline and Troughs' ||
+    feature.properties.PROJ_NAME === 'Ski Protection Fence and Cattleguard' ||
+    feature.properties.PROJ_NAME === 'Petan Piute Basin Fence Gate' ||
+    feature.properties.PROJ_NAME === 'Rabbit Creek Pipeline' ||
+    feature.properties.PROJ_NAME === 'Windy Point Pipeline' ||
+    feature.properties.PROJ_NAME === 'Wilson Creek Pipeline' ||
+    feature.properties.PROJ_NAME === 'CHOKECHERRY PIPELINE'
+  )
+}
 
-const filterFeatures = (feature) => 
-  feature.properties.PROJ_NAME === 'Shares Basin Fence' ||
-  feature.properties.PROJ_NAME === 'Jim Sage Cattleguard' || 
-  feature.properties.PROJ_NAME === 'Robber Gulch Pipeline and Troughs' ||
-  feature.properties.PROJ_NAME === 'Ski Protection Fence and Cattleguard' ||
-  feature.properties.PROJ_NAME === 'Petan Piute Basin Fence Gate' ||
-  feature.properties.PROJ_NAME === 'Rabbit Creek Pipeline' ||
-  feature.properties.PROJ_NAME === 'Windy Point Pipeline' ||
-  feature.properties.PROJ_NAME === 'Wilson Creek Pipeline' ||
-  feature.properties.PROJ_NAME === 'CHOKECHERRY PIPELINE'
-
-// TODO REMOVE this is diagnostic code
-const filtered_feat_points = axios('blm_idaho_range_improvement_point.geojson')
-  .then(resp => {
-    console.log(resp.data)
-    let filteredData = resp.data.features.filter(filterFeatures)
-    console.log('filteredData >> ', filteredData)
-    filteredData.forEach(feature => {
-        console.log('feat_point >> ', feature.properties.POINT_FEAT)
-      })
-  })
-
-// source: https://github.com/pointhi/leaflet-color-markers
 const pointFeatColors = {
-    'AIR VALVE': 'gold',
-    'CATTLEGUARD': 'green',
-    'DRAIN': 'violet',
-    'GATE': 'red',
-    'PRESSURE BREAK': 'black',
-    'SPRING': 'orange',
-    'TROUGH': 'blue',
-    'VALVE': 'yellow',
+  'AIR VALVE': 'gold',
+  'CATTLEGUARD': 'green',
+  'DRAIN': 'violet',
+  'GATE': 'red',
+  'PRESSURE BREAK': 'black',
+  'SPRING': 'orange',
+  'TROUGH': 'blue',
+  'VALVE': 'yellow',
 }
 
-// https://github.com/pointhi/leaflet-color-markers
 const createColorIcon = (color) => {
-    return new L.Icon({
-        iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    })
+  return new L.Icon({
+    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
 }
 
-const range_imps = axios('blm_idaho_range_improvement_point.geojson')
-    .then(resp => {
-        console.log(resp.data)
-        L.geoJSON(resp.data, {
-            style: { color: '#116ad2' },
-            filter: filterFeatures,
-            pointToLayer: (feature, latlng) => {
-                let pointFeatColor = pointFeatColors[feature.properties.POINT_FEAT]
-                // fallback color
-                if (!pointFeatColor) {
-                    pointFeatColor = 'grey'
-                }
-                // create colored icon
-                let colorIcon = createColorIcon(pointFeatColor)
-                // use colored icon when creating marker
-                let new_marker = L.marker(latlng, { icon: colorIcon })
-                new_marker.bindPopup(
-                    '<b>' + feature.properties.PROJ_NAME + '</b><br>' + feature.properties.POINT_FEAT)
-                return new_marker
-            }
+let geoJsonLayer
 
-        }).addTo(map)
+function drawMap(data) {
+  if (geoJsonLayer) {
+    map.removeLayer(geoJsonLayer)
+  }
+  geoJsonLayer = L.geoJSON(data, {
+    style: { color: '#116ad2' },
+    filter: filterFeatures,
+    pointToLayer: (feature, latlng) => {
+      let pointFeatColor = pointFeatColors[feature.properties.POINT_FEAT]
+      if (!pointFeatColor) {
+        pointFeatColor = 'grey'
+      }
+      let colorIcon = createColorIcon(pointFeatColor)
+      let new_marker = L.marker(latlng, { icon: colorIcon })
+      new_marker.bindPopup(
+        'Name: ' 
+        +'<b>' + toTitleCase(feature.properties.PROJ_NAME) + '</b><br>' 
+        + 'Feature: ' + '<b>' + toTitleCase(feature.properties.POINT_FEAT)
+        + '</b>')
+      return new_marker
+    }
+  }).addTo(map)
+}
 
-        console.log('finished loading')
+function toTitleCase(word) {
+  return word.toLowerCase().split(' ').map((new_word) => {
+    return(new_word.charAt(0).toUpperCase() + new_word.slice(1))
+  }).join(' ')
+}
+
+axios('blm_idaho_range_improvement_point.geojson')
+  .then(resp => {
+    drawMap(resp.data)
+    console.log('finished loading')
+    document.querySelectorAll('#filters input[type="checkbox"]').forEach(function (checkbox) {
+      checkbox.addEventListener('change', function () {
+        if (this.checked) {
+          activeFilters.add(this.value)
+        } else {
+          activeFilters.delete(this.value)
+        }
+        drawMap(resp.data)
+      })
     })
-
-    
-
+  })
